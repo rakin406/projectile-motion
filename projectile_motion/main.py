28fdc43c6e14d08fe7c0bd1def2c60a4be40bad7
rakin406 <rakinhacker@gmail.com>
2024-06-06 20:54:34 +0600

    feat: display ground image

--- projectile_motion/main.py
+++ projectile_motion/main.py
@@ -2,12 +2,17 @@
 
 import pyglet
 
+pyglet.resource.path = ["../assets"]
+pyglet.resource.reindex()
+
 window = pyglet.window.Window()
+ground = pyglet.resource.image("ground.png")
 
 
 @window.event
 def on_draw():
     window.clear()
+    ground.blit(0, 0)
 
 
 pyglet.app.run()
