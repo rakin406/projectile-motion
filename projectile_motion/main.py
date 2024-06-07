099bf9daede5eded8fa506b1c48950c3806c5ed3
rakin406 <rakinhacker@gmail.com>
2024-06-07 20:35:40 +0600

    feat: draw white background

--- projectile_motion/main.py
+++ projectile_motion/main.py
@@ -1,18 +1,25 @@
 """Entry point to the simulation."""
 
 import pyglet
+from pyglet.window import Window
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
 pyglet.resource.path = ["../assets"]
 pyglet.resource.reindex()
 
-window = pyglet.window.Window()
-ground = pyglet.resource.image("ground.png")
+window = Window(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, caption="Projectile Motion")
+
+background = pyglet.image.SolidColorImagePattern(
+    (255, 255, 255, 255)).create_image(WINDOW_WIDTH, WINDOW_HEIGHT)
+# cannon = pyglet.resource.image("cannon.png")
 
 
 @window.event
 def on_draw():
     window.clear()
-    ground.blit(0, 0)
+    background.blit(0, 0)
 
 
 pyglet.app.run()
