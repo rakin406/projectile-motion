95cc51a60bd76a95394a5dd7ec7ab54a6dd91c12
rakin406 <rakinhacker@gmail.com>
2024-06-07 21:13:45 +0600

    feat: draw a straight floor

--- projectile_motion/main.py
+++ projectile_motion/main.py
@@ -5,14 +5,23 @@ from pyglet.window import Window
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+FLOOR_HEIGHT = WINDOW_HEIGHT - 600
 
 pyglet.resource.path = ["../assets"]
 pyglet.resource.reindex()
 
-window = Window(width=WINDOW_WIDTH, height=WINDOW_HEIGHT, caption="Projectile Motion")
+window = Window(width=WINDOW_WIDTH, height=WINDOW_HEIGHT,
+                caption="Projectile Motion")
+batch = pyglet.graphics.Batch()
 
+# Create white background
 background = pyglet.image.SolidColorImagePattern(
     (255, 255, 255, 255)).create_image(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+# Create a straight solid floor
+floor = pyglet.shapes.Line(x=0, y=FLOOR_HEIGHT, x2=1280, y2=FLOOR_HEIGHT,
+                           width=5, color=(0, 0, 0), batch=batch)
+
 # cannon = pyglet.resource.image("cannon.png")
 
 
@@ -20,6 +29,7 @@ background = pyglet.image.SolidColorImagePattern(
 def on_draw():
     window.clear()
     background.blit(0, 0)
+    batch.draw()
 
 
 pyglet.app.run()
