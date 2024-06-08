e0715f40e8e602db6139d386f90488f5a39cd201
rakin406 <rakinhacker@gmail.com>
2024-06-08 21:05:51 +0600

    refactor: use cannon.width and bucket.width

--- projectile_motion/main.py
+++ projectile_motion/main.py
@@ -3,6 +3,7 @@
 import math
 import pyglet
 from pyglet.window import Window
+from pyglet import shapes
 from pyglet.window import key
 from pyglet.window import mouse
 
@@ -29,29 +30,36 @@ background = pyglet.image.SolidColorImagePattern(
     (255, 255, 255, 255)).create_image(WINDOW_WIDTH, WINDOW_HEIGHT)
 
 # Create a straight solid floor
-floor = pyglet.shapes.Line(x=0, y=FLOOR_HEIGHT, x2=1280, y2=FLOOR_HEIGHT,
-                           width=5, color=(0, 0, 0), batch=batch)
+floor = shapes.Line(x=0, y=FLOOR_HEIGHT, x2=1280, y2=FLOOR_HEIGHT,
+                    width=5, color=(0, 0, 0), batch=batch)
 
 # Create cannon sprite
 # WARNING: y-axis of the cannon is hardcoded.
 cannon_image = pyglet.resource.image("cannon.png")
-cannon = pyglet.sprite.Sprite(
-    cannon_image, x=10, y=FLOOR_HEIGHT - 21, batch=batch)
+cannon = pyglet.sprite.Sprite(cannon_image, batch=batch)
 cannon.scale = 0.1
+cannon.x = 10
+cannon.y = FLOOR_HEIGHT - 21
+cannon_center_x = cannon.x + (cannon.width / 2)
+
+# Create cannonball sprite
+cannonball_image = pyglet.resource.image("cannonball.png")
+cannonball = pyglet.sprite.Sprite(cannonball_image, x=10, y=FLOOR_HEIGHT - 21)
+cannonball.scale = 0.15
 
 # Create bucket sprite
 # WARNING: y-axis of the bucket is hardcoded.
 bucket_image = pyglet.resource.image("bucket.png")
-bucket = pyglet.sprite.Sprite(
-    bucket_image, x=WINDOW_WIDTH // 2, y=FLOOR_HEIGHT + 3, batch=batch)
+bucket = pyglet.sprite.Sprite(bucket_image, batch=batch)
 bucket.scale = 0.2
+bucket.x = (WINDOW_WIDTH // 2) - (bucket.width / 2)
+bucket.y = FLOOR_HEIGHT + 3
 bucket.dx = 400.0
 
 
 def get_displacement_x() -> int:
     """Get horizontal displacement."""
-    cannon_center_x = cannon.x + ((cannon_image.width / 2) * cannon.scale)
-    bucket_center_x = bucket.x + ((bucket_image.width / 2) * bucket.scale)
+    bucket_center_x = bucket.x + (bucket.width / 2)
     return math.ceil(bucket_center_x - cannon_center_x)
 
 
@@ -67,9 +75,9 @@ label = pyglet.text.Label(TEXT.format(distance=get_displacement_x()),
 
 @window.event
 def on_key_press(symbol, _):
+    # Start the simulation
     if symbol == key.SPACE:
-        # TODO: Start the simulation.
-        pass
+        cannonball.batch = batch
     elif symbol == key.R:
         # TODO: Implement reset mechanism.
         pass
@@ -83,10 +91,9 @@ def on_draw():
 
 
 def move_bucket(dt):
-    if mousebuttons[mouse.LEFT] and \
-            bucket.x > (cannon.x + (cannon_image.width * cannon.scale)):
+    if mousebuttons[mouse.LEFT] and bucket.x > (cannon.x + cannon.width):
         bucket.x -= bucket.dx * dt
-    elif mousebuttons[mouse.RIGHT] and (bucket.x + (bucket_image.width * bucket.scale)) < window.get_size()[0]:
+    elif mousebuttons[mouse.RIGHT] and (bucket.x + bucket.width) < window.get_size()[0]:
         bucket.x += bucket.dx * dt
 
     # Update the text
